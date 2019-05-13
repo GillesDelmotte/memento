129b5258e00d027bdc95436ca7ce27f70752a857
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-13 09:25:51 +0200

    add appointment controller/model/migration/seeder

---
+++ database/factories/AppointmentFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use memento\Appointment;
+use Faker\Generator as Faker;
+
+$factory->define(Appointment::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
