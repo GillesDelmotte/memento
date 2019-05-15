5b822e28da5a0cf694afafbdc1edc454dd5b6a10
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-15 14:24:24 +0200

    add admin component

---
+++ resources/js/components/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="admin">Admin component</section>
+</template>
+
+<script>
+import store from "../store.js";
+import { mapMutations } from "vuex";
+export default {
+  name: "Admin",
+  beforeMount() {
+    store.commit("setComponentDisplayed", "Admin");
+  }
+};
+</script>
+
+<style scoped>
+</style>
