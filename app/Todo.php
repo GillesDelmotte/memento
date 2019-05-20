260de9698af0407044c1e770e975a2215741751f
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-20 16:59:51 +0200

    Update Todo.php

--- app/Todo.php
+++ app/Todo.php
@@ -4,7 +4,16 @@ namespace memento;
 
 use Illuminate\Database\Eloquent\Model;
 
+use memento\Appointment;
+
 class Todo extends Model
 {
-    //
+    protected $fillable = [
+        'appointment_id', 'when'
+    ];
+
+    public function appointment()
+    {
+        return $this->belongsTo(Appointment::class, 'appointment_id');
+    }
 }
