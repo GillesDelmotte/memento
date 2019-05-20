54ff9041e45adb74eab2d233a7ece43967262b7c
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-20 16:59:12 +0200

    Create reminder.blade.php

---
+++ resources/views/email/reminder.blade.php
@@ -0,0 +1,6 @@
+<h1>
+    Bonjour {{$user->name}}
+</h1>
+<div>
+    N'oubliez pas votre rendez-vous avec {{$praticien->name}} le {{$todo->appointment->date}} à {{$todo->appointment->hour}} ! 
+</div>
