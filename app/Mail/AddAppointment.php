d784f077ad8a7ca031ef216c8bfeb0c0bb1f1337
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-20 14:13:45 +0200

    create class addAppointment ( mail )

---
+++ app/Mail/AddAppointment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace memento\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class AddAppointment extends Mailable
+{
+    use Queueable, SerializesModels;
+    protected $user;
+    protected $hour;
+    protected $date;
+    protected $practitien;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $hour, $date, $practitien)
+    {
+        $this->user = $user;
+        $this->hour = $hour;
+        $this->date = $date;
+        $this->practitien = $practitien;
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('email.addAppointment', ['user' => $this->user, 'hour' => $this->hour, 'date' => $this->date, 'practitien' => $this->practitien]);
+    }
+}
