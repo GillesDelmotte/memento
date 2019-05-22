599f30a17b66fecf5b0d79ad39f10dd55058e039
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-22 14:45:46 +0200

    Update AuthenticatedUserController.php

--- app/Http/Controllers/AuthenticatedUserController.php
+++ app/Http/Controllers/AuthenticatedUserController.php
@@ -3,12 +3,15 @@
 namespace memento\Http\Controllers;
 
 use Illuminate\Http\Request;
+use memento\FilesUpload;
 
 class AuthenticatedUserController extends Controller
 {
     public function fetch(){
         $user = auth()->user();
 
-        return $user->load('job');
+        $image = FilesUpload::where('user_id', auth()->id())->first();
+
+        return ['user' => $user->load('job'), 'img' => $image];
     }
 }
