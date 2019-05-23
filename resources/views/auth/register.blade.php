943b5ffb4dc59b3fae1e9cf9f0a088323223554e
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-23 13:38:38 +0200

    fix design

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
         @csrf
         <div class="registerform__name">
             <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-            <label for="name" class="col-md-4 col-form-label text-md-right">Nom&nbsp;:</label>
+            <label for="name" >Nom&nbsp;:</label>
 
                 @if ($errors->has('name'))
                     <span class="invalid-feedback" role="alert">
@@ -18,8 +18,8 @@
         </div>
 
         <div class="registerform__email">
-            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autocomplete="email">
-            <label for="email" class="col-md-4 col-form-label text-md-right">Adresse email&nbsp;:</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required autocomplete="email">
+            <label for="email" >Adresse email&nbsp;:</label>
 
                 @if ($errors->has('email'))
                     <span class="invalid-feedback" role="alert">
@@ -28,9 +28,19 @@
                 @endif
         </div>
 
+        <div class="registerform__gsm">
+            <input id="gsm" type="tel"  pattern="[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}" name="gsm"  required>
+            <label for="gsm" >Numéro de gsm&nbsp;:</label>
+        </div>
+
+        <div class="registerform__job">
+            <input id="job" type="text" name="job" required>
+            <label for="job" >Profession&nbsp;:</label>
+        </div>
+
         <div class="registerform__password">
             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required autocomplete="new-password">
-            <label for="password" class="col-md-4 col-form-label text-md-right">Mot de passe&nbsp;:</label>
+            <label for="password" >Mot de passe&nbsp;:</label>
 
                 @if ($errors->has('password'))
                     <span class="invalid-feedback" role="alert">
@@ -42,7 +52,7 @@
 
         <div class="registerform__confirm">
             <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirmation de mot de passe&nbsp;:</label>
+            <label for="password-confirm" >Confirmation de mot de passe&nbsp;:</label>
         </div>
 
         <button type="submit" class="registerform__submit__button button">
