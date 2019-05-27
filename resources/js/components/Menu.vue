0daee971188f22c4cc9d7d56d3e288234a0f6c4f
gilles delmotte <gilles.delmotte@outlook.be>
2019-05-27 16:16:09 +0200

    add animation on menu

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="container menu">
-    <a @click="redirect($event, 'scheduleList')">Mes agendas</a>
-    <a v-if="this.currentUser.is_admin" @click="redirect($event, 'adminStat')">admin</a>
-    <a @click="redirect($event, 'profile')">Mon profil</a>
-    <a @click="logout">Déconnexion</a>
+    <a @click="redirect($event, 'scheduleList')" ref="agenda">Mes agendas</a>
+    <a v-if="this.currentUser.is_admin" @click="redirect($event, 'adminStat')" ref="admin">admin</a>
+    <a @click="redirect($event, 'profile')" ref="profil">Mon profil</a>
+    <a @click="logout" ref="logout">Déconnexion</a>
   </section>
 </template>
 
@@ -31,6 +31,33 @@ export default {
       e.stopPropagation();
       router.push({ name: name });
     }
+  },
+  mounted() {
+    const { agenda, admin, profil, logout } = this.$refs;
+
+    const tl = new TimelineMax();
+
+    tl.from(agenda, 0.3, { autoAlpha: 0, top: 50, ease: Power2.easeInOut });
+    if (admin) {
+      tl.from(
+        admin,
+        0.3,
+        { autoAlpha: 0, top: 50, ease: Power2.easeInOut },
+        "-=0.2"
+      );
+    }
+
+    tl.from(
+      profil,
+      0.3,
+      { autoAlpha: 0, top: 50, ease: Power2.easeInOut },
+      "-=0.2"
+    ).from(
+      logout,
+      0.3,
+      { autoAlpha: 0, top: 50, ease: Power2.easeInOut },
+      "-=0.2"
+    );
   }
 };
 </script>
